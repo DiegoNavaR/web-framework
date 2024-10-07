1e1d17e50e1ac1d5acbcce7a26470e1bfb0e4a9c
diegojose.navarro <diegojose150@gmail.com>
2024-10-07 11:46:08 -0500

    allure-screenshots-implementation

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -4,9 +4,14 @@ import core.utils.DriverManager;
 import core.utils.LoggerUtils;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
 import io.qameta.allure.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.io.ByteArrayInputStream;
 
 public class Hooks {
 
@@ -26,6 +31,23 @@ public class Hooks {
     @Description("This method tears down the WebDriver after each scenario.")
     public void teardown() {
         LoggerUtils.logInfo("Tearing down driver");
+
+        takeScreenshot();
+
         DriverManager.getInstance().quitDriver();
+    }
+
+    @Step("Taking screenshot")
+    private void takeScreenshot() {
+        try {
+            WebDriver driver = DriverManager.getInstance().getDriver();
+            if (driver instanceof TakesScreenshot) {
+                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                Allure.addAttachment("Screenshot", "image/png", new ByteArrayInputStream(screenshot), ".png");
+                LoggerUtils.logInfo("Screenshot captured and added to Allure report.");
+            }
+        } catch (Exception e) {
+            LoggerUtils.logError("Failed to capture screenshot: " + e.getMessage());
+        }
     }
 }
